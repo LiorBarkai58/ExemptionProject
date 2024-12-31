dd3b914a702ed2c801b01a8118f6128bb1abead0
Cornetto <lior5858@gmail.com>
2024-12-31 12:11:49 +0200

    Add variable locking

--- MultithreadingAsync/SupportCenter.cs
+++ MultithreadingAsync/SupportCenter.cs
@@ -20,16 +20,23 @@ public class SupportCenter {
 
     private Queue<Customer> customers;//The customers in the center
 
+    private readonly object CustomerLock = new();
+
     public SupportCenter(){
         customers = new Queue<Customer>();
     }
 
     public void AddCustomer(Customer customer){//Adds a customer to the support center queue
-        customers.Enqueue(customer);
+        lock (CustomerLock)
+        {
+            customers.Enqueue(customer);    
+        }
+        
     }
 
     public void StartProcessingAll(){//Starts processing all of the customers that are currently in the queue
         while(customers.Count > 0){
+
             if(thread1 == null || !thread1.IsAlive){//Checks if the thread is done/available
                 thread1 = new Thread(ProcessRequest);//Creates a new thread
                 thread1.Start();//Starts the work
@@ -38,11 +45,15 @@ public class SupportCenter {
             if(thread2 == null || !thread2.IsAlive){//If thread 1 was unavailable runs the same as thread2
                 thread2 = new Thread(ProcessRequest);
                 thread2.Start();
+                continue;
             }
             if(thread3 == null || !thread3.IsAlive){//If thread 1 was unavailable runs the same as thread2
                 thread3 = new Thread(ProcessRequest);
                 thread3.Start();
+                continue;
             }
+
+            Thread.Sleep(50);//incase all 3 threads are busy, give a little bit of time between interations to reduce load on cpu.
             //While isAlive also shows when a thread is ready but has not started yet, I didn't initialize the therads with the constructor but rather initialize them and start them immediatly
             //therefore there is never a time when they are ready but get overrun.
             
@@ -51,7 +62,12 @@ public class SupportCenter {
     }
 
     private void ProcessRequest(){
-        Customer current = customers.Dequeue();//Takes the first customer and dequeues it
+        Customer current = null;
+        
+        lock(CustomerLock){
+            if(customers.Count <= 0) return;
+            current = customers.Dequeue();
+        }
 
         Console.WriteLine($"Processing {current.Name}");
         if(current.SpecialRequest){
--- MultithreadingAsync/obj/Debug/net8.0/MultithreadingAsync.AssemblyInfo.cs
+++ MultithreadingAsync/obj/Debug/net8.0/MultithreadingAsync.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultithreadingAsync")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d7fb0786caf008884f37ae0fb049e255392dfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultithreadingAsync")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultithreadingAsync")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
